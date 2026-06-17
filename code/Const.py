00cc8f2a6cf67fbdfb91c3cc76c51f46bfabcdca
keroline1212 <kerolineluthke@gmail.com>
2026-06-17 20:00:32 -0300

    Creatng players and enemies Moving players and enemies

--- code/Const.py
+++ code/Const.py
@@ -1,4 +1,6 @@
 # C
+import pygame
+
 COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255, 255, 255)
 COLOR_YELLOW = (255, 255, 0)
@@ -12,7 +14,14 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
+    'Player1': 3,
+    'Player2': 3,
+    'Enemy1': 2,
+    'Enemy2': 1,
 }
+
+# E
+EVENTY_ENEMY = pygame.USEREVENT + 1
 
 # M
 MENU_OPTION = ('NEW GAME 1P',
@@ -21,6 +30,18 @@ MENU_OPTION = ('NEW GAME 1P',
                'SCORE',
                'EXIT')
 
+PLAYER_KEY_UP = {'Player1': pygame.K_UP,
+                 'Player2': pygame.K_w}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
+                   'Player2': pygame.K_s}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
+                   'Player2': pygame.K_a}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
+                    'Player2': pygame.K_d}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
+                    'Player2': pygame.K_LCTRL}
+# s
+SPAWN_TIME = 4000
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
--- code/Entity.py
+++ code/Entity.py
@@ -5,6 +5,7 @@ import pygame
 
 class Entity(ABC):
 
+
     def __init__(self, name: str, position: tuple):
         self.name = name
         self.surf = pygame.image.load('./asset/' + name + '.png').convert_alpha()
